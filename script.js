d1f235c3d0d59179a48d2d5954c2303c64275049
Esther-Guo <special0831qi@gmail.com>
2021-10-23 12:36:42 +0800

    toggle active states

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const questions = document.querySelectorAll('.question-board');
+const questionDivs = document.querySelectorAll('.question-board');
+const questions = document.querySelectorAll('.question');
 const answers = document.querySelectorAll('.answer')
 let states = [0,0,0,0,0];
 
 function displayAnswer() {
     const idx = Number(this.dataset.index);
-    let questionDiv = questions[idx];
+    let questionDiv = questionDiv[idx];
     let answer = answers[idx];
     if (states[idx] == 0) {
         questionDiv.querySelector('.question').style.fontWeight = 'bold';
@@ -20,5 +21,15 @@ function displayAnswer() {
     }
 }
 
-
-questions.forEach(question => question.addEventListener('click', displayAnswer));
+questionDivs.forEach(question => question.addEventListener('click', displayAnswer));
+questions.forEach(question => question.addEventListener('mouseover', (e) => {
+    const idx = Number(e.target.dataset.index);
+    let q = questions[idx];
+    q.style.color = 'hsl(14, 88%, 65%)';
+}));
+questions.forEach(question => question.addEventListener('mouseout', (e) => {
+    
+    const idx = Number(e.target.dataset.index);
+    let q = questions[idx];
+    q.style.color = 'hsl(237, 12%, 33%)';
+}));
